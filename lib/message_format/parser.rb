b87463b3683bf6ff10d38994e38ebef3a1f780a4
Nathan Froyd <froydnj@gmail.com>
2024-01-17 17:17:50 -0500

    match digits with a regex

--- lib/message_format/parser.rb
+++ lib/message_format/parser.rb
@@ -37,16 +37,7 @@ module MessageFormat
     end
 
     def is_digit ( char )
-      char == '0' or
-      char == '1' or
-      char == '2' or
-      char == '3' or
-      char == '4' or
-      char == '5' or
-      char == '6' or
-      char == '7' or
-      char == '8' or
-      char == '9'
+      char.match?(/[0123456789]/)
     end
 
     def is_whitespace ( char )
